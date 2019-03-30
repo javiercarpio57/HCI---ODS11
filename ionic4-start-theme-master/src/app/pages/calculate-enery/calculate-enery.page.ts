9bd3a591dbdeab6fd79365e457ca595df2c0e02c
Javier Carpio <javierrr.carpio57@gmail.com>
2019-03-30 00:53:33 -0600

    Muchos muchos avances

---
+++ ionic4-start-theme-master/src/app/pages/calculate-enery/calculate-enery.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-calculate-enery',
+  templateUrl: './calculate-enery.page.html',
+  styleUrls: ['./calculate-enery.page.scss'],
+})
+export class CalculateEneryPage implements OnInit {
+
+  datos = null;
+  potenciaE = null;
+  cantPanel = null;
+
+  constructor(private route: ActivatedRoute, public navCtrl: NavController) { }
+
+  ngOnInit() {
+    this.datos = this.route.snapshot.paramMap.get('myLista');
+    this.datos = parseFloat(this.datos)/1000;
+    this.datos = this.datos.toFixed(2)
+    this.potenciaElectrica();
+    this.cantPaneles();
+    console.log("Calculo: " + this.datos);
+  }
+
+  potenciaElectrica(){
+    this.potenciaE = parseFloat(this.datos) * 1200 / 5;
+    this.potenciaE = parseFloat(this.potenciaE).toFixed(2);
+  }
+
+  cantPaneles(){
+    this.cantPanel = parseFloat(this.potenciaE)/265;
+    this.cantPanel = parseFloat(this.cantPanel).toFixed(2);
+  }
+
+  goToMenu() {
+    this.navCtrl.navigateForward('/home-results');
+  }
+
+}
