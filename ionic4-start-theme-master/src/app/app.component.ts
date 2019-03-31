9b88f49aec7393d24381e6e98f58c822d50e1834
Javier Carpio <javierrr.carpio57@gmail.com>
2019-03-31 10:55:07 -0600

    Diseno terminado

--- ionic4-start-theme-master/src/app/app.component.ts
+++ ionic4-start-theme-master/src/app/app.component.ts
@@ -40,6 +40,12 @@ export class AppComponent {
         url: '/settings',
         direct: 'forward',
         icon: 'globe'
+      },
+      {
+        title: 'Estadísticas',
+        url: '/home-results',
+        direct: 'root',
+        icon: 'stats'
       }
     ];
 
