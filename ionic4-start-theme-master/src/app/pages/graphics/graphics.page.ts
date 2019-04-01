830bd8f0f777efec7f51e67957bce57d7cb4bcb6
Javier Carpio <javierrr.carpio57@gmail.com>
2019-03-31 19:06:17 -0600

    Graficas listas

--- ionic4-start-theme-master/src/app/pages/graphics/graphics.page.ts
+++ ionic4-start-theme-master/src/app/pages/graphics/graphics.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-graphics',
@@ -7,9 +8,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphicsPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('earth') earth;
+  lineChart: any;
+
+  @ViewChild('panel') panel;
+  lineChart2: any;
+
+  dates: any[] = [];
+  earths: any[] = [];
+
+  dates2: any[] = [];
+  paneles: any[] = [];
+
+  constructor() {
+    
+    //this.dates.push("31/03/2019");
+    //this.earths.push(5);
+
+    // this.dates2.push("31/03/2019");
+    //this.paneles.push(21);
+   }
 
   ngOnInit() {
+    this.graphic();
+    this.graphic2();
+  }
+  
+  private graphic(){
+    this.lineChart = new Chart(this.earth.nativeElement, {
+      type: 'line',
+      data: {
+        labels: this.dates,
+        datasets: [
+          {
+            label: "Cantidad de planetas Tierra",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "rgb(0, 0, 0)",
+            pointBorderWidth: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.earths,
+          }
+        ]
+      },
+      options:{
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+                max: 5,
+                min: 0,
+                stepSize: 1
+            }
+          }]
+        }
+      }
+    });
   }
 
+  private graphic2(){
+    this.lineChart2 = new Chart(this.panel.nativeElement, {
+      type: 'line',
+      data: {
+        labels: this.dates2,
+        datasets: [
+          {
+            label: "Cantidad de páneles solares",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "rgb(0, 0, 0)",
+            pointBorderWidth: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.paneles,
+          }
+        ]
+      },
+      options:{
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+                suggestedMax: 20,
+                min: 0,
+                stepSize: 2
+            }
+          }]
+        }
+      }
+    });
+  }
 }
